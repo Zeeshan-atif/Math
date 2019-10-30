a2bde9c66e7331cc16070e05bc732285d37b6af4
Zeeshan <zishan10@gmail.com>
2019-10-30 11:47:55 +0100

    inital commit

---
+++ MyMath/MyUnitTest/BasicOperationsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyPrograms;
+
+
+namespace MyUnitTest
+{
+    [TestClass]
+    public class BasicOperationsTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            BasicOperations basicOperations = new BasicOperations();
+            int actualResult = basicOperations.Add(2,3);
+            int expectedResult = 5;
+            Assert.AreEqual(expectedResult,actualResult);
+
+        }
+        [TestMethod]
+        public void TestMul()
+        {
+            BasicOperations basicOperations = new BasicOperations();
+            int expectedResult = 6;
+            int actualResult = basicOperations.Mul(2, 3);
+         
+            Assert.AreEqual(expectedResult,actualResult);
+
+        }
+    }
+}
---
+++ MyMath/MyUnitTest/ClassicProgramTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyPrograms;
+
+namespace MyUnitTest
+{
+
+    [TestClass]
+    public class ClassicProgramTest
+    {
+        
+           [TestMethod]
+                public void TestIsEven()
+                {
+            ClassicPrograms classicPrograms = new ClassicPrograms();
+            bool expectedResult = true;
+            bool actualResult = classicPrograms.IsEven(8);
+            Assert.AreEqual(expectedResult, actualResult);
+                }
+        [TestMethod]
+        public void TestIsNotEven()
+        {
+            ClassicPrograms classicPrograms = new ClassicPrograms();
+            bool expectedResult = false;
+            bool actualResult = classicPrograms.IsEven(7);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestNotPrime_withNegativeValue()
+        {
+            ClassicPrograms classicPrograms = new ClassicPrograms();
+            bool expectedResult = false;
+            bool actualResult = classicPrograms.IsPrime(-7);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestIsPrime_withTruePrimeValue()
+        {
+            ClassicPrograms classicPrograms = new ClassicPrograms();
+            bool expectedResult = true;
+            bool actualResult = classicPrograms.IsPrime(7);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestIsPrime_withFalsePrimeValue()
+        {
+            ClassicPrograms classicPrograms = new ClassicPrograms();
+            bool expectedResult = false;
+            bool actualResult = classicPrograms.IsPrime(-7);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
